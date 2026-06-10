cbdd2f2089a006bf8ba3a1fe6f3135cd6b2faa53
ncyzzZ <88054319+ncyzzZ@users.noreply.github.com>
2026-06-10 22:46:21 +0700

    Create CustomWorld class for Cucumber integration
    Implement a custom world for Cucumber tests with additional properties.

---
+++ support/world.js
@@ -0,0 +1,15 @@
+const { setWorldConstructor, World } = require('@cucumber/cucumber');
+
+class CustomWorld extends World {
+  constructo
+      super(options);
+          this.browser = null;
+              this.context = null;
+                  this.page = null;
+                      this.apiResponse = null;
+                          this.createdEmployeeId = null;
+                              this.savedId = null;
+                                }
+                                }
+
+                                setWorldConstructor(CustomWorld);
