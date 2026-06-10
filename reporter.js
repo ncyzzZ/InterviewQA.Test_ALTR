e1aafc52142aa3f6383747d79c1b5bb1099d69b5
ncyzzZ <88054319+ncyzzZ@users.noreply.github.com>
2026-06-10 23:59:50 +0700

    Modify reporter options for cucumber-html-reporter
    Updated the jsonFile property to jsonDir and changed the Executed metadata to 'GitHub Actions'.

--- reporter.js
+++ reporter.js
@@ -1,19 +1,19 @@
 const reporter = require('cucumber-html-reporter');
 
 const options = {
-    theme: 'bootstrap',
-    jsonFile: 'reports/cucumber_report.json',
-    output: 'reports/cucumber_report.html',
-    reportSuiteAsScenarios: true,
-    scenarioTimestamp: true,
-    launchReport: false,
-    metadata: {
-          'App Version': '1.0.0',
-          'Test Environment': 'QA Practice',
-          'Browser': 'Chromium (Playwright)',
-          'Parallel': 'Scenarios',
-          'Executed': 'Local'
-        }
-  };
+  theme: 'bootstrap',
+  jsonDir: 'reports/json',
+  output: 'reports/cucumber_report.html',
+  reportSuiteAsScenarios: true,
+  scenarioTimestamp: true,
+  launchReport: false,
+  metadata: {
+    'App Version': '1.0.0',
+    'Test Environment': 'QA Practice',
+    'Browser': 'Chromium (Playwright)',
+    'Parallel': 'Scenarios',
+    'Executed': 'GitHub Actions'
+  }
+};
 
 reporter.generate(options);
